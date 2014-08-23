0f31f57ba04217e7c2ec4fa252efc9882b3e493e
Josh Cheek <josh.cheek@gmail.com>
2014-08-23 15:07:46 -0600

    Fix test bug identified by @evanpx

--- test/test_hello.rb
+++ test/test_hello.rb
@@ -2,7 +2,7 @@ require 'turingbot/test_case'
 
 class TestHelloCommand < Turingbot::TestCase
   def test_hello
-    response = run_command "hello there", 'user_name' => 'tester'
-    assert_equal "back attacha' tester", response.body["text"]
+    response = run_command "hello there", from: 'some-user'
+    assert_equal "back attacha' some-user", response.body["text"]
   end
 end
