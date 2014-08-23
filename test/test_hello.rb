97a16a5fe20eb86ae808c5f3af79dcfbd22af538
Josh Cheek <josh.cheek@gmail.com>
2014-08-23 12:04:55 -0600

    Be explicit about username in test that depends on it

--- test/test_hello.rb
+++ test/test_hello.rb
@@ -2,7 +2,7 @@ require 'turingbot/test_case'
 
 class TestHelloCommand < Turingbot::TestCase
   def test_hello
-    response = inject "hello there"
+    response = inject "hello there", 'user_name' => 'tester'
     assert_equal "back attacha' tester", response.body["text"]
   end
 end
