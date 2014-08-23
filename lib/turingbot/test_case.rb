a30f47a5d7f9e61099ae6d55aab0e3130796a23b
Josh Cheek <josh.cheek@gmail.com>
2014-08-23 13:16:38 -0600

    Extract params_for from run_command

--- lib/turingbot/test_case.rb
+++ lib/turingbot/test_case.rb
@@ -13,19 +13,18 @@ module Turingbot
     end
 
     def run_command(msg, opts={})
-      vars = {
-        "text"         => msg,
+      response = Response.new
+      Turingbot::Command.run(params_for(msg, opts), response)
+      response
+    end
+
+    def params_for(msg, opts={})
+      { "text"         => msg,
         "user_name"    => opts.fetch(:from, "tester"),
         "channel_name" => opts.fetch(:channel, "test"),
         "channel_id"   => "aabbcc",
         "token"        => "xxyyzz"
       }
-
-      response = Response.new
-
-      Turingbot::Command.run(vars, response)
-
-      response
     end
   end
 end
