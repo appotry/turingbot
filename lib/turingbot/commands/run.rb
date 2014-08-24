9629900c96f594a64aaf76d0a028683afdf48384
Jonmichael Chambers <jon@tyrbo.net>
2014-08-24 13:37:45 -0600

    Kernel has several methods that might be abusable. I am removing it for now.

--- lib/turingbot/commands/run.rb
+++ lib/turingbot/commands/run.rb
@@ -9,7 +9,6 @@ module Turingbot
         priv.methods_of(Fixnum).allow_all
         priv.methods_of(Hash).allow_all
         priv.methods_of(Array).allow_all
-        priv.methods_of(Kernel).allow_all
         priv.methods_of(Class).allow :new
         priv.methods_of(Module).allow :new
       end
